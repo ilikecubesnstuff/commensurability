7e4d207a5f85f06316465a88a41e142ff8da21b3
ilikecubesnstuff <25328250+ilikecubesnstuff@users.noreply.github.com>
2024-03-02 18:13:18 +0000

    :fire: Remove all outdated example scripts.

--- examples/demo/5-saved.py
+++
@@ -1,8 +0,0 @@
-# package imports
-from commensurability import TessellationAnalysis2D
-
-# read in analysis object
-tanal = TessellationAnalysis2D.read_from_hdf5("5-data.hdf5")
-
-# scroll & click to explore the phase space
-tanal.launch_interactive_plot("x", "vy")
--- examples/demo/6-iplot.py
+++
@@ -1,8 +0,0 @@
-# package imports
-from commensurability import TessellationAnalysis
-
-# read in analysis object
-tanal = TessellationAnalysis.read_from_hdf5("6-iplot-demo.hdf5")
-
-# scroll & click to explore the phase space
-tanal.launch_interactive_plot("x", "vy")
