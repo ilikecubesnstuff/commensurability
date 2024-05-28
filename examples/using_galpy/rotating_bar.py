8bf2d046c0ee373df6cb1aa74785e9af2489aa2a
michael-petersen <petersen.michael.s@gmail.com>
2024-05-28 15:55:01 +0100

    add multiprocessing protection

--- examples/using_galpy/rotating_bar.py
+++ examples/using_galpy/rotating_bar.py
@@ -45,10 +45,13 @@ def ic_function(x, vy, z):
 
 dt = 0.01 * u.Gyr
 steps = 500
-tanal = TessellationAnalysis(
-    ic_function, values, pot, dt, steps, pattern_speed=omega, pidgey_chunksize=50
-)
-tanal.launch_interactive_plot("x", "vy")
 
-tanal.save(f"examples/using_galpy/bar_{SIZE}_{FRAMES}.hdf5")
-tanal.launch_interactive_plot("x", "vy")
+
+if __name__ == '__main__':
+    tanal = TessellationAnalysis(
+        ic_function, values, pot, dt, steps, pattern_speed=omega, pidgey_chunksize=50
+    )
+    tanal.launch_interactive_plot("x", "vy")
+
+    tanal.save(f"examples/using_galpy/bar_{SIZE}_{FRAMES}.hdf5")
+    tanal.launch_interactive_plot("x", "vy")
